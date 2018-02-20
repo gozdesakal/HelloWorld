9c1366fbe6c94b9d348cc8add1f8d2015da20ca0
Gözde Sakal <36489037+gzdskl@users.noreply.github.com>
2018-02-20 11:53:36 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+# Hello World program
+
+#!bin/python3
+
+#Insert code below this line to output Hello World
+
+print("Hello World")
+
+#Declare a variable that has your name as the value
+
+name="Gözde"
+
+#Output Hello followed by your name
+
+print("Hello "+name)
+
+#Accept name from the user and store it in a variable
+
+name=input("Enter your name: ")
+yas=input("Yaşınız:")
+
+#Output Hello followed by the user-defined name
+
+print ("Hello, " + name)
+print("Yaşınız: "+yas)
+
+
+
+x=5+6
+print(x)
